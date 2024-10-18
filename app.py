a2819aa028c359881e707ffab1af6719827f3831
Ryan Guidry (Ryne) <inquireryan@gmail.com>
2024-10-18 00:13:18 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -28,4 +28,6 @@ def generate_idea():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+ ##   app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
